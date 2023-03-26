7e08e1033b7d16c4093577cd362199378a4e2a45
sainamercy <sainamercy@gmail.com>
2023-03-26 22:15:50 +0300

    landing page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,21 +1,21 @@
 
 function Navbar() {
   return (
-    <nav class="nav">
+    <nav className="nav">
     <img
       src="./src/assets/logof.png"
       alt=" logo"
-      class="nav__logo"
+      className="nav__logo"
     />
-    <ul class="nav__links">
-      <li class="nav__item">
-        <a class="nav__link" href="#">Tasks</a>
+    <ul className="nav__links">
+      <li className="nav__item">
+        <a className="nav__link" href="#">Tasks</a>
       </li>
-      <li class="nav__item">
-        <a class="nav__link" href="#">Login</a>
+      <li className="nav__item">
+        <a className="nav__link" href="#">Login</a>
       </li>
-      <li class="nav__item">
-        <a class="nav__link nav__link--btn " href="#"
+      <li className="nav__item">
+        <a className="nav__link nav__link--btn " href="#"
           >Get Started</a>
       </li>
     </ul>
