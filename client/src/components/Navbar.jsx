e17d23f81c3261f2e773996bcbcc0f6c7b9bc8b9
sainamercy <sainamercy@gmail.com>
2023-05-26 12:31:20 +0300

    responsive nav

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,20 +1,28 @@
 import { Link } from "react-router-dom";
 import network from "../utils/network";
 import { getToken } from "../utils/auth";
+import { useState } from "react";
+
 function Navbar() {
+  const [active, setActive] = useState(false);
+
   const onlogout = () => {
     network.logout();
     window.location.reload();
     window.location.href = "/";
   };
   const token = getToken();
+
+  const handleNav = () => {
+    setActive(!active);
+  };
   return (
     <nav className="nav">
       <Link to="/">
         <img src="/assets/logof.png" alt=" logo" className="nav__logo" />
       </Link>
-      <i className="fa-solid fa-bars ham"></i>
-      <ul className="nav__links">
+      <i className="fa-solid fa-bars ham" onClick={handleNav}></i>
+      <ul className={`nav__links ${!active && "hidden"}`} onClick={handleNav}>
         <li className="nav__item">
           <Link className="nav__link" to="/">
             Home
